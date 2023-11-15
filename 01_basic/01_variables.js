a2bea064866202d3137f708cf10250dd3c9f05cc
mdkaifraza42661 <kaifraza42661@gmail.com>
2023-11-15 12:32:57 +0530

    varibles

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,1 +1,14 @@
-console.log("hi this is offline work")
+const accountId = 2;
+let accountEmail = "google@gmail.com"
+var accountpassword = "123564";
+accountcity = "bokaro"
+
+let accountState
+/*prefer not to use var because of block and fumctional scope */
+
+//accountId = 6; not allowed
+console.log(accountId);// type of TypeError: Assignment to constant variable.
+accountEmail = "kaif@gmail.com0";
+accountpassword = "52648";
+accountcity = "kollkata"
+console.table([accountEmail, accountpassword, accountcity, accountId, accountState]);
