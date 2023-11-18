9cba94b62e5612be9c22b78be665b1d12e48d8d4
mdkaifraza42661 <kaifraza42661@gmail.com>
2023-11-18 12:48:43 +0530

    someextra

---
+++ 01_basic/03_convert_varible.js
@@ -0,0 +1,23 @@
+// let score = 33;
+
+// check type of varibles
+// console.log(typeof (score));
+
+//convert the type of the varibles
+
+/*let changeType = String(score)
+console.log(typeof (changeType));*/
+
+/*let score = "33aaa";
+
+console.log(typeof (score));
+
+
+let convertstring = Number(score)
+
+console.log(convertstring); */ // output NaN
+
+
+// "33"=>33;
+// "333aaa"=> NaN
+// "true=>1,"flase" => 0
