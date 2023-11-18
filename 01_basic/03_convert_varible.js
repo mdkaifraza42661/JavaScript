fd230d0ba706e932d261d3cf1787c71d8dc16583
mdkaifraza42661 <kaifraza42661@gmail.com>
2023-11-18 20:21:22 +0530

    comparsion or understanding

--- 01_basic/03_convert_varible.js
+++ 01_basic/03_convert_varible.js
@@ -21,3 +21,27 @@ console.log(convertstring); */ // output NaN
 // "33"=>33;
 // "333aaa"=> NaN
 // "true=>1,"flase" => 0
+
+
+// *****************************operation *******************//
+// let value = 3
+// let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+
+
+
+let str1 = "kaif";
+let str2 = " hi"
+let concate = str1 + str2
+console.log(concate);
+
+
+console.log(+true); //1
+console.log(true); //true
---
+++ 01_basic/04_comparisiondatatype.js
@@ -0,0 +1,11 @@
+// console.log(2 > 2);// true or flase boolean value
+// console.log(2 >= 2);// true or flase boolean value
+// console.log(2 < 2);// true or flase boolean value
+// console.log(2 == 2);// true or flase boolean value
+// console.log(2 != 2);// true or flase boolean value
+
+
+
+console.log(null > 0); // false
+console.log(null == 0);// false
+console.log(null >= 0);//true
