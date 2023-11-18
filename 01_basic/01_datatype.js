04e323b412ffc1db741a5870b518ae1e878303bf
mdkaifraza42661 <kaifraza42661@gmail.com>
2023-11-18 11:57:10 +0530

    disscuse object

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,17 @@
+// "use strict" // treat all js code as newer version
+// alert("hellow")// we are using nodejs not browers show error
+
+
+// type of datatype
+// 1 string "kaif"
+// 2 number 4545;
+// 3 boolean true/flase
+// null is standolon value
+// undefined means define varible but assign the value
+//symbole = unique
+
+
+// object
+// console.log(typeof(varible name));
+// null type  hota hai object
+// undefined type hota hai undefined  
