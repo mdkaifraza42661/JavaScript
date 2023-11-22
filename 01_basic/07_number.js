bffb8fe02a26e88d4f9311c858f1e7c9ef3cb98c
mdkaifraza42661 <kaifraza42661@gmail.com>
2023-11-22 12:34:44 +0530

    NUMBER AND MATH

---
+++ 01_basic/07_number.js
@@ -0,0 +1,66 @@
+/*const num = 400;
+console.log(num);
+
+const numAcount = new Number(100)
+console.log(typeof numAcount);
+console.log(numAcount);
+//convert number to string
+
+console.log(numAcount.toString());
+// to fixesd its mean to roundoff the amount
+
+console.log(numAcount.toFixed(2));
+
+const othernumber = 22.566
+console.log(othernumber.toPrecision(3));
+
+
+const curency = 1000000;
+
+console.log(curency.toLocaleString(en - IN)); // convert the local currency */
+
+
+
+
+
+
+
+
+
+// **********************************Math ************************************
+
+
+
+
+/*console.log(Math);
+
+console.log(Math.abs(-4)); // absulute value that convert to negative value to positive but not positive to negative
+
+console.log(Math.round(4333.6333));// its convert to roundoff number
+
+console.log(Math.ceil(42.3)); //(ceiling roof) it convert to upper roundoff
+
+console.log(Math.floor(42.3)); // base  it convert to lower roundoff*/
+
+
+// console.log(Math.random());// math.random is return 0.235665812848 value 
+
+// console.log((Math.random() * 10) + 1);//(Math.random() * 10) + 1  if you multiply into 10 its return 0 to 10  and what is one one is basically its does not return 0 value
+
+
+// console.log(Math.floor(Math.random() * 10) + 1); // if you add floor its return lest value of the  output
+
+
+
+
+// if you need to 10 to 20 value 
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); //BODMAS (20-10+1)
+
+
+
+
+
+
