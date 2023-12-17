d1e6829b75bc8bd9ecdae522035cef1b53755531
mdkaifraza42661 <kaifraza42661@gmail>
2023-12-17 21:10:08 +0530

    funt indeep

---
+++ 03_basic/02-func.js
@@ -0,0 +1,24 @@
+/// shoping cart
+
+// parameter add
+
+
+function CalculateCart(...num1) // rest operator
+{
+    return num1
+}
+// console.log(CalculateCart(500, 200, 6, 88));
+
+
+
+const UserId = {
+    UaerName: "kaif",
+    age: 25
+}
+
+function objectuser(objectvl) {
+    console.log(` Name : ${objectvl.UaerName}  and age:  ${objectvl.age}`);
+}
+objectuser(UserId)
+
+
