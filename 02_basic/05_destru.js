ab05eee9183f2c55be8a1866302c53e7c9d9b70f
mdkaifraza42661 <kaifraza42661@gmail>
2023-12-17 19:50:23 +0530

    destructuring

---
+++ 02_basic/05_destru.js
@@ -0,0 +1,16 @@
+// destructuring concept
+
+const course = {
+    Course: "destruct",
+    Price: 555,
+    CourceInstructure: "kaif"
+}
+// course.CourceInstructure
+console.log(course.Price);
+const {Course} = course
+
+console.log(Course);
+
+
+
+
