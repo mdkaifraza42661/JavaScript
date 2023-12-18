cdad33ac863c52fedef0bd48fce8d360c17561d9
mdkaifraza42661 <kaifraza42661@gmail>
2023-12-18 13:25:32 +0530

    funtion

---
+++ 03_basic/03_scope.js
@@ -0,0 +1,24 @@
+let a = 30;
+const b = 3;
+var c = 10
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// goloble scope
+// local scope
+
+
+
+
+function addno(num1) {
+    return num1 + 1
+}
+
+addno(4)
+
+
+
+const addtwo = function(num2) {
+    return num2 + 1
+}
+addtwo(5)
