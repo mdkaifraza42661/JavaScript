5e4d9112f4694f7cd319b00c1a7c1a69f3fc4920
mdkaifraza42661 <kaifraza42661@gmail>
2023-12-19 11:32:23 +0530

    while&dowhile

---
+++ 05_itterator/02_whiledo.js
@@ -0,0 +1,26 @@
+// while (condition) {
+
+// }
+
+// while(2 == 2) {
+//     console.log("true");
+// }
+// i++
+// let i = 0;
+// while(i < 10) {
+//     console.log(i);
+//     i = i + 2
+// }
+
+// let Superhero = ["batman", "spiderman", "ironman"]
+// let arr = 0;
+// while(arr < Superhero.length) {
+//     console.log(`SuperHeroName is ${Superhero[arr]}`);
+//     arr = arr + 1
+// }
+let Superhero = ["batman", "spiderman", "ironman"]
+let score = 0;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while(score <= 10);
